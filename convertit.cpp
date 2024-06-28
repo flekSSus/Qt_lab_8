a8fb9a023a349c9cecc6d8a41f38dd779777c630
Vova <zayac.zayac.w@gmail.com>
2024-06-28 16:48:49 +0300

    Two LineEdits & button| GridBox
    Added two LineEdits and one button using gridBox
    but i dont know how to change distnce between box elements

--- convertit.cpp
+++ convertit.cpp
@@ -2,5 +2,25 @@
 
 ConvertIt::ConvertIt(QWidget *parent):QWidget(parent)
 {
+    _pMainWidget=new QWidget;
 
+    _pLeftConversionText=new QLineEdit;
+    _pRightConversionText=new QLineEdit;
+    _midChangeButton=new QPushButton("<=>");
+    _pGridLayout= new QGridLayout(_pMainWidget);
+
+    //_pLeftConversionText->resize(10,100);
+    _pGridLayout->addWidget(_pLeftConversionText,2,1);
+    _pLeftConversionText->setMaximumWidth(40);
+
+    _pGridLayout->addWidget(_pRightConversionText,2,4);
+
+    _pGridLayout->addWidget(_midChangeButton,2,3);
+    _pGridLayout->addWidget(nullptr,2,2);
+
+    this->resize(100,100);
+    this->setSizePolicy(QSizePolicy::Fixed,QSizePolicy::Fixed);
+    this->setMinimumSize(500,300);
+    this->setMaximumSize(500,300);
+    this->setLayout(_pGridLayout);
 }
--- convertit.h
+++ convertit.h
@@ -1,12 +1,20 @@
 #ifndef CONVERTIT_H
 #define CONVERTIT_H
 #include<QWidget>
+#include<QLineEdit>
+#include<QGridLayout>
+#include<QPushButton>
 
 
 class ConvertIt:public QWidget
 {
 public:
     ConvertIt(QWidget* parent=nullptr);
+    QWidget *_pMainWidget;
+    QLineEdit *_pLeftConversionText;
+    QLineEdit *_pRightConversionText;
+    QPushButton *_midChangeButton;
+    QGridLayout *_pGridLayout;
 
 
 };
